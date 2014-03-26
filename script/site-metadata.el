ed80da963b5f92ef1414ec22d1bc2ca60f46d070
KDr2 <killy.draw@gmail.com>
2014-03-26 22:09:02 +0800

    artical id update

--- script/site-metadata.el
+++ script/site-metadata.el
@@ -1,4 +1,4 @@
 ;;(time, id, file, title)
-("2014-03-26 Sun 21:43" r5rs-features "tech/emacs/orgmode-export-process.org" "Orgmode Export Process")
+("2014-03-26 Sun 21:43" orgmode-export-process "tech/emacs/orgmode-export-process.org" "Orgmode Export Process")
 ("2014-01-05 Sun 13:43" r5rs-features "tech/lisp/r5rs-features.org" "R^{5}RS Features List")
 ("2010-12-14 Fri 10:33" pyc-format "tech/python/pyc-format.org" "PYC文件格式分析")
