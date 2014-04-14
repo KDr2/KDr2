7026aab6a122023843c9d906209298e85ddc3200
KDr2 <killy.draw@gmail.com>
2014-04-14 15:20:55 +0800

    css update and test file update

--- script/site-org-conf.el
+++ script/site-org-conf.el
@@ -8,15 +8,6 @@
 
 (setq org-html-checkbox-type 'unicode)
 
-(defun sacha/org-html-checkbox (checkbox)
-  "Format CHECKBOX into HTML."
-  (case checkbox (on "<span class=\"check\">&#x2611;</span>") ; checkbox (checked)
-        (off "<span class=\"checkbox\">&#x2610;</span>")
-        (trans "<code>[-]</code>")
-        (t "")))
-(defadvice org-html-checkbox (around sacha activate)
-  (setq ad-return-value (sacha/org-html-checkbox (ad-get-arg 0))))
-
 (defadvice org-html-paragraph (before org-html-paragraph-advice
                                       (paragraph contents info) activate)
   "Join consecutive Chinese lines into a single long line without
