fdd5631b6040973d27f39e6209b9a72a6b2e78bf
KDr2 <killy.draw@gmail.com>
2014-06-12 16:41:54 +0800

    new post, sleepd bugfix

--- script/site-metadata.el
+++ script/site-metadata.el
@@ -1,12 +1,13 @@
 ;;(time lang id file title)
+("2014-06-11 Wed 19:16" en d1406-bugfix-sleepd-batcap "tech/linux/1406-sleepd-bug-batt-cap.org"
+ "Bugfix: Bad battery percentage reporting in sleepd")
 ("2014-05-30 Fri 14:41" zh d1405-monthly-review "introspect/monthly-review-1405.org" "Monthly Review: 2014-05")
 ("2014-05-23 Fri 14:52" en d1405-org-unicode-checkbox "tech/emacs/1405-orgmode-checkbox-unicode.org"
  "Unicode Characters for Checkbox in Orgmode-Exported HTML")
 ("2014-05-22 Thu 19:54" zh d1405-contrib-to-gnu "tech/attic/1405-contributing-to-gnu.org" "Contributing to FSF/GNU")
 ("2014-05-04 Sun 18:24" zh d1405-labor-day "life/1405-a-true-labor-day.org" "名符其实的劳动节")
 ("2014-04-30 Wed 21:24" zh d1404-monthly-review "introspect/monthly-review-1404.org" "Monthly Review: 2014-04")
-("2014-04-19 Sat 16:24" zh d1404-dialectical "jotting/1404-dialectical-thinking-ruins-my-soup.org"
- "辩证思维毁了我的羊杂汤")
+("2014-04-19 Sat 16:24" zh d1404-dialectical "jotting/1404-dialectical-thinking-ruins-my-soup.org" "辩证思维毁了我的羊杂汤")
 ("2014-04-07 Mon 20:44" zh d1404-mounth-ulcer "life/1404-mounth-ulcer.org" "口腔溃疡处理方法")
 ("2014-04-03 Thu 15:43" en bug-ob-scheme-in-batch-mode "tech/emacs/ob-scheme-in-batch-mode.org"
  "Babel Bug: execute scheme code blocks in batch mode")
