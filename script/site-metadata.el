5f65ea3765a9ba2205cbe059a41d9d211c0df608
KDr2 <killy.draw@gmail.com>
2014-08-01 15:26:18 +0800

    new post: mr 1407

--- script/site-metadata.el
+++ script/site-metadata.el
@@ -1,4 +1,5 @@
 ;;(time lang id file title)
+("2014-07-31 Thu 19:27" 中 d1407-monthly-review "introspect/monthly-review-1407.org" "Monthly Review: 2014-07")
 ("2014-07-23 Wed 08:09" 中 d1407-birthday-gifts "life/1407-birthday-gifts.org" "Birthday gifts, 2014")
 ("2014-07-15 Tue 15:01" en d1407-docker-issues "tech/linux/1407-docker-issues-i-met.org" " Docker issues I met")
 ("2014-07-02 Wed 17:01" en d1407-julia-types "tech/julia/1407-julia-types-summary.org" "Julia Types Summary")
