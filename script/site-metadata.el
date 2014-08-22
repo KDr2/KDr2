0f820cd82aec928849efc7a1fc8d3c99998ea946
KDr2 <killy.draw@gmail.com>
2014-08-22 22:44:19 +0800

    new post: julia v0.3

--- script/site-metadata.el
+++ script/site-metadata.el
@@ -1,4 +1,5 @@
 ;;(time lang id file title)
+("2014-08-22 Fri 22:14" en d1408-julia-v0.3 "tech/julia/1408-julia-v0.3-released.org" "Julia 0.3.0 Final")
 ("2014-08-10 Sun 09:43" en d1408-docker-img-4-mch-demo "tech/linux/1408-docker-img-for-mch-demo.org" "A Docker Image for Moochine Demo")
 ("2014-07-31 Thu 19:27" 中 d1407-monthly-review "introspect/monthly-review-1407.org" "Monthly Review: 2014-07")
 ("2014-07-23 Wed 08:09" 中 d1407-birthday-gifts "life/1407-birthday-gifts.org" "Birthday gifts, 2014")
