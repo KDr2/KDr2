a90e9670352f58a516fb33cffa5841438c64ff51
KDr2 <killy.draw@gmail.com>
2015-03-01 15:47:32 +0800

    new post: mr 1502

--- script/site-metadata.el
+++ script/site-metadata.el
@@ -1,4 +1,5 @@
 ;;(time lang id file title)
+("2015-02-28 Sat 20:16" 中 d1502-monthly-review "introspect/monthly-review-1502.org" "Monthly Review: 2015-02")
 ("2015-02-18 Wed 18:09" 中 d1502-new-year "life/1502-new-year.org" "春节 2015")
 ("2015-01-31 Sat 20:58" 中 d1501-monthly-review "introspect/monthly-review-1501.org" "Monthly Review: 2015-01")
 ("2015-01-13 Tue 19:54" en d1501-stap-bug17696-fixed "tech/linux/1501-stap-bug17696-fixed.org" "Systemtap: Bug 17696 is fixed")
