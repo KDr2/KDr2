37d5ae24e9ceb1bd4863c96d3f4f43f11c5c3100
KDr2 <killy.draw@gmail.com>
2015-06-30 21:30:11 +0800

    new post: mr 1506

--- script/site-metadata.el
+++ script/site-metadata.el
@@ -1,4 +1,5 @@
 ;;(time lang id file title)
+("2015-06-30 Tue 21:17" 中 d1506-monthly-review "introspect/monthly-review-1506.org" "Monthly Review: 2015-06")
 ("2015-05-31 Sun 22:36" 中 d1505-monthly-review "introspect/monthly-review-1505.org" "Monthly Review: 2015-05")
 ("2015-04-30 Thu 22:46" 中 d1504-monthly-review "introspect/monthly-review-1504.org" "Monthly Review: 2015-04")
 ("2015-03-31 Tue 22:11" 中 d1503-monthly-review "introspect/monthly-review-1503.org" "Monthly Review: 2015-03")
